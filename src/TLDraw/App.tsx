3413823da4b1ac58cbda4984854efd406fec74d9
GlennHenaresII <glennhenaresii@gmail.com>
2023-10-08 20:06:05 +0800

    added remaining react files

---
+++ src/TLDraw/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Draw from './components/Draw'
+
+function App() {
+    return (
+        <div>
+            <Draw />
+        </div>
+    )
+}
+
+export default App
